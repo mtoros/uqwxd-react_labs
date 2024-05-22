c9d2d18f93cb93ee8e7f8995f08992393ac93ba7
mtoros <mtoros@gmail.com>
2024-05-22 10:58:14 +0200

    Fixed bugs

--- react-redux-master/src/components/MyButton.js
+++ react-redux-master/src/components/MyButton.js
@@ -1,1 +1,13 @@
 
+import React from 'react'
+import { useDispatch} from 'react-redux';
+import increment from '../actions'
+
+const MyButton = ()=>{
+    let dispatch = useDispatch();
+    return (
+        <button onClick={()=>dispatch(increment(5))}>Increase counter</button>
+    );
+}
+
+export default MyButton;
--- react-redux-master/src/index.js
+++ react-redux-master/src/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+//import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import App from './App';
 // import * as serviceWorker from './serviceWorker';
 
+//import {configureStore} from 'redux';
 import {createStore} from 'redux';
 import {Provider} from 'react-redux'
 import myReducers from './reducers'
@@ -10,15 +12,20 @@ import myReducers from './reducers'
 
 //Create the store
 const myStore = createStore(myReducers);
+//const myStore = configureStore(myReducers);
 
 //This will console log the current state everytime the state changes
 myStore.subscribe(()=>console.log(myStore.getState()));
 
 //Enveloping the App inside the Provider, ensures that the states in the store are available
 //throughout the application
-ReactDOM.render(<Provider store={myStore}><App/></Provider>, document.getElementById('root'));
+const domNode = document.getElementById('root');
+const root = createRoot(domNode);
+root.render(<Provider store={myStore}><App/></Provider>);
+//ReactDOM.render(<Provider store={myStore}><App/></Provider>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 // serviceWorker.unregister();
+
